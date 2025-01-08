6454e24d3e5b25ef7b1e3adda0971e47b03290e3
lavindu perera <lavi156perera@gmail.com>
2025-01-08 23:42:13 +0530

    more styles to workflow

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { WorkflowStatus } from "@/types/workflow";
 import { Workflow } from "@prisma/client";
 import { FileTextIcon, PlayIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const statusColors = {
@@ -17,17 +18,32 @@ function WorkflowCard({ workflow }: { workflow: Workflow }) {
   return (
     <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primary/30">
       <CardContent className="p-4 items-center justify-between h-[100px]">
-        <div
-          className={cn(
-            "w-10 h-10 rounded-full flex items-center  justify-center",
-            statusColors[workflow.status as WorkflowStatus]
-          )}
-        >
-          {isDraft ? (
-            <FileTextIcon className="h-5 w-5" />
-          ) : (
-            <PlayIcon className="h-5 w-5 text-white " />
-          )}
+        <div className="flex items-center justify-end space-x-3 gap-4">
+          <div
+            className={cn(
+              "w-10 h-10 rounded-full flex items-center  justify-center",
+              statusColors[workflow.status as WorkflowStatus]
+            )}
+          >
+            {isDraft ? (
+              <FileTextIcon className="h-5 w-5" />
+            ) : (
+              <PlayIcon className="h-5 w-5 text-white " />
+            )}
+          </div>
+          <div>
+            <h3 className="text-base font-bold text-muted-foreground flex items-center">
+              <Link
+                href={`/workflow/editor/${workflow.id}`}
+                className="flex items-center hover:underline"
+              >
+                {workflow.name}
+              </Link>
+              {isDraft && (
+                <span className="ml-2 px-2 py-0.5 text-xs font-medium bg-yellow-800 rounded-full"></span>
+              )}
+            </h3>
+          </div>
         </div>
       </CardContent>
     </Card>
