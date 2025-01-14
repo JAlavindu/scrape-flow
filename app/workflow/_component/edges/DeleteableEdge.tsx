0b1e85acfd36c08c4dfd3dba3170058fc1637d64
lavindu perera <lavi156perera@gmail.com>
2025-01-14 12:49:42 +0530

    add deletable button for edges

--- app/workflow/_component/edges/DeleteableEdge.tsx
+++ app/workflow/_component/edges/DeleteableEdge.tsx
@@ -1,9 +1,20 @@
 "use client";
 
-import { BaseEdge, EdgeProps, getSmoothStepPath } from "@xyflow/react";
+import { Button } from "@/components/ui/button";
+import {
+  BaseEdge,
+  EdgeLabelRenderer,
+  EdgeProps,
+  getSimpleBezierPath,
+  getSmoothStepPath,
+  Position,
+  useReactFlow,
+} from "@xyflow/react";
+import { transform } from "next/dist/build/swc";
 
 export default function DeletableEdge(props: EdgeProps) {
-  const [edgePath] = getSmoothStepPath(props);
+  const [edgePath, labelX, labelY] = getSimpleBezierPath(props);
+  const { setEdges } = useReactFlow();
   return (
     <>
       <BaseEdge
@@ -11,6 +22,26 @@ export default function DeletableEdge(props: EdgeProps) {
         markerEnd={props.markerEnd}
         style={props.style}
       />
+      <EdgeLabelRenderer>
+        <div
+          style={{
+            Position: "absolute",
+            transform: `translate(-50%, -50%) translate(${labelX}px, ${labelY}px)`,
+            pointerEvents: "all",
+          }}
+        >
+          <Button
+            variant={"outline"}
+            size={"icon"}
+            className="w-5 h-5 border cursor-pointer rounded-full text-xs leading-none hover:shadow-lg"
+            onClick={() => {
+              setEdges((edges) => edges.filter((edge) => edge.id !== props.id));
+            }}
+          >
+            X
+          </Button>
+        </div>
+      </EdgeLabelRenderer>
     </>
   );
 }
