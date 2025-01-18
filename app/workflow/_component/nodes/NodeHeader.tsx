4ba4d38dcd14cd13baf6be5ed226eccca8f8ab81
lavindu perera <lavi156perera@gmail.com>
2025-01-18 23:59:10 +0530

    add more node options

--- app/workflow/_component/nodes/NodeHeader.tsx
+++ app/workflow/_component/nodes/NodeHeader.tsx
@@ -2,13 +2,23 @@
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
 import { TaskRegistry } from "@/lib/workflow/task/registy";
+import { AppNode } from "@/types/appNode";
 import { TaskType } from "@/types/task";
+import { useReactFlow } from "@xyflow/react";
 import { CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon } from "lucide-react";
 import React from "react";
 
-function NodeHeader({ taskType }: { taskType: TaskType }) {
+function NodeHeader({
+  taskType,
+  nodeId,
+}: {
+  taskType: TaskType;
+  nodeId: string;
+}) {
   const task = TaskRegistry[taskType];
+  const { deleteElements, getNode, addNodes } = useReactFlow();
   return (
     <div className="flex items-center gap-2 p-2">
       <task.icon size={16} />
@@ -24,11 +34,32 @@ function NodeHeader({ taskType }: { taskType: TaskType }) {
           </Badge>
           {!task.isEntryPoint && (
             <>
-              <Button variant={"ghost"} size={"icon"}>
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                onClick={() =>
+                  deleteElements({
+                    nodes: [{ id: nodeId }],
+                  })
+                }
+              >
                 <TrashIcon size={20} />
               </Button>
 
-              <Button variant={"ghost"} size={"icon"}>
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                onClick={() => {
+                  const node = getNode(nodeId) as AppNode;
+                  const newX = node.position.x;
+                  const newY = node.position.y + node.measured?.height! + 20;
+                  const newNode = CreateFlowNode(node.data.type, {
+                    x: newX,
+                    y: newY,
+                  });
+                  addNodes([newNode]);
+                }}
+              >
                 <CopyIcon size={20} />
               </Button>
             </>
@@ -38,7 +69,7 @@ function NodeHeader({ taskType }: { taskType: TaskType }) {
             size={"icon"}
             className="drag-handle cursor-grab"
           >
-            <GripVerticalIcon size={20} />
+            <GripVerticalIcon size={12} />
           </Button>
         </div>
       </div>
