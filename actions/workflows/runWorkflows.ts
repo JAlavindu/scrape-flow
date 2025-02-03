a11ab3d1b3c38e20437760494911e3ba81776355
lavindu perera <lavi156perera@gmail.com>
2025-02-03 10:13:36 +0530

    adding more functions for nodes

--- actions/workflows/runWorkflows.ts
+++ actions/workflows/runWorkflows.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
 import { WorkflowExecutionPlan } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 import { Workflow } from "lucide-react";
@@ -26,8 +27,25 @@ export async function RunWorkflow(form: {
     },
   });
 
-  let executuonPlan: WorkflowExecutionPlan;
-  if (!flowDefinition) {
-    throw new Error("Flow definition is required");
+  if (!workflow) {
+    throw new Error("Workflow not found");
   }
+
+  let executionPlan: WorkflowExecutionPlan;
+  if (!flowDefinition) {
+    throw new Error("Flow definition is not defined");
+  }
+
+  const flow = JSON.parse(flowDefinition);
+  const result = FlowToExecutionPlan(flow.nodes, flow.edges);
+  if (result.error) {
+    throw new Error("fow definition is invalid");
+  }
+
+  if (!result.executionPlan) {
+    throw new Error("Execution plan is not defined");
+  }
+
+  executionPlan = result.executionPlan;
+  console.log("Execution plan", executionPlan);
 }
--- app/workflow/_component/topbar/ExecuteBtn.tsx
+++ app/workflow/_component/topbar/ExecuteBtn.tsx
@@ -1,20 +1,47 @@
 "use client";
 
+import { RunWorkflow } from "@/actions/workflows/runWorkflows";
 import useExecutionPlan from "@/components/hooks/useExecutionPlan";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { useReactFlow } from "@xyflow/react";
 import { PlayIcon } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 function ExecuteBtn({ workflowId }: { workflowId: string }) {
   const generate = useExecutionPlan();
+  const { toObject } = useReactFlow();
+
+  const mutation = useMutation({
+    mutationFn: RunWorkflow,
+    onSuccess: () => {
+      toast.success("Workflow executed successfully", {
+        id: "flow-exectution",
+      });
+    },
+    onError: (error) => {
+      toast.error("Something went wrong", { id: "flow-exectution" });
+    },
+  });
   return (
     <Button
       variant={"outline"}
       className="flex items-center gap-2"
+      disabled={mutation.isPending}
       onClick={() => {
         const plan = generate();
-        console.log("--plan--");
-        console.table(plan);
+        if (!plan) {
+          toast.error("Execution plan is not defined", {
+            id: "flow-exectution",
+          });
+          return;
+        }
+
+        mutation.mutate({
+          workflowId,
+          flowDefinition: JSON.stringify(toObject()),
+        });
       }}
     >
       <PlayIcon size={16} className="stroke-orange-400" />
